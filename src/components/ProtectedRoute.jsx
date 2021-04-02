77d98ac0e5bf919070903155151bca434e14afeb
Tomas Hidalgo <tomashcordero@gmail.com>
2021-04-03 10:40:42 +1300

    Add _redirects file in public folder

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -12,7 +12,7 @@ import Auth from "./Auth";
     
 
     return <Route {...rest} render={props => Auth.isAuthenticated ? (<Component {...props}/>) :
-        <Redirect to="https://wonderful-brown-78efce.netlify.app/" />} />
+        <Redirect to="/" />} />
 
 }
 
