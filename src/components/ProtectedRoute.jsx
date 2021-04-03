33e740c95815747c39e25b49d45020b68561d0b3
Tomas Hidalgo <tomashcordero@gmail.com>
2021-04-04 07:29:49 +1200

    Checking auth functions

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -10,7 +10,9 @@ import Auth from "./Auth";
 
 function ProtectedRoute({ component: Component, ...rest }) {
     
-    console.log(Auth.isAuthenticated);
+    
+    console.log(Auth.getAuth().then(result => result));
+    console.log(Auth.getAuthUser().then(result => result));
 
     return <Route {...rest} render={props => Auth.isAuthenticated ? (<Component {...props} />) :
         <Redirect to="/" />} />
